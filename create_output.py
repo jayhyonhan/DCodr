ba15a23f71848c91b5b8c157257fa49efa949e2f
Japple <jayhyonhan@gmail.com>
2023-12-23 10:55:36 +0000

    Finished creating caesar cipher and the script to create the outputs

--- create_output.py
+++ create_output.py
@@ -1,5 +1,9 @@
 import datetime
+from os.path import exists
+from os import makedirs
 def create_output(output, cipher):
+    if not(exists("./outputs/")):
+        makedirs("./outputs/")
     file_path = ""
     now = datetime.datetime.now()
     file_path = ("./outputs/%s_%s.txt"%(cipher, now.strftime("%Y-%m-%d-%H-%M-%S-%f")))
